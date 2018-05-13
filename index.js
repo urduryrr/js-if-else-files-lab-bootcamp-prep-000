f9dcbaeda40f5a5faa2d47cd15c1ce343a6a3d0b
Urður Ýrr Brynjólfsdóttir <urduryrr@gmail.com>
2018-05-13 13:46:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var name = "Joe"
 var height = "74"
 
+if (name === "Susan"){
+  console.log("The name is correct")
+}
+else {
+  console.log("The name is incorrect")
+}
 
 // Don't worry about this
 module.exports = { name, height
